86aef94793e039457f141e81a1b0fa7dbafbd131
BabaYaga0179 <hungnth@yj5t7.onmicrosoft.com>
2024-10-29 15:52:13 +0700

    Update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const directadminConfig = `#Auth: wptop.net
+const directadminConfig = `cp /usr/local/directadmin.bak-old/conf/cacert.pem /usr/local/directadmin/conf/cacert.pem
+cp /usr/local/directadmin.bak-old/conf/cakey.pem /usr/local/directadmin/conf/cakey.pem
+cp /usr/local/directadmin.bak-old/conf/carootcert.pem /usr/local/directadmin/conf/carootcert.pem
 
 cp /usr/local/directadmin/conf/directadmin.conf /usr/local/directadmin/conf/directadmin_1.conf
 truncate -s 0 /usr/local/directadmin/conf/directadmin.conf
-cat <<EOF >> /usr/local/directadmin/conf/directadmin.conf
+cat <<EOF > /usr/local/directadmin/conf/directadmin.conf
 add_userdb_quota=1
 admin_ssl_check_retries=0
 apache_public_html=0
@@ -23,6 +25,7 @@ ethernet_dev=lo:100
 frontpage_on=0
 hide_brute_force_notifications=1
 http2=1
+ip={ip}
 ipv6=0
 jail=1
 letsencrypt=1
@@ -54,11 +57,9 @@ zstd=1
 EOF
 `;
 
-const optionsConfig = `#Auth: wptop.net
-
-cp /usr/local/directadmin/custombuild/options.conf /usr/local/directadmin/custombuild/options_1.conf
+const optionsConfig = `cp /usr/local/directadmin/custombuild/options.conf /usr/local/directadmin/custombuild/options_1.conf
 truncate -s 0 /usr/local/directadmin/custombuild/options.conf
-cat <<EOF >> /usr/local/directadmin/custombuild/options.conf
+cat <<EOF > /usr/local/directadmin/custombuild/options.conf
 #WEB Server Settings
 php1_release={php1_release}
 php1_mode=lsphp
@@ -172,11 +173,9 @@ csf=no
 EOF
 `;
 
-const setupConfig = `#Auth: wptop.net
-
-cp /usr/local/directadmin/scripts/setup.txt /usr/local/directadmin/scripts/setup_1.txt
+const setupConfig = `cp /usr/local/directadmin/scripts/setup.txt /usr/local/directadmin/scripts/setup_1.txt
 truncate -s 0 /usr/local/directadmin/scripts/setup.txt
-cat <<EOF >> /usr/local/directadmin/scripts/setup.txt
+cat <<EOF > /usr/local/directadmin/scripts/setup.txt
 hostname={redirect_host}
 email=contact@{redirect_host}
 mysql=0d7db9ac1c1d09494dcf
@@ -193,16 +192,154 @@ services=services_es70_64.tar.gz
 litespeedadmin=J6dxMaMTgIl7SR
 EOF
 
+cp /usr/local/directadmin/data/admin/ip.list /usr/local/directadmin/data/admin/ip.list_1
+truncate -s 0 /usr/local/directadmin/data/admin/ip.list
+cat <<EOF > /usr/local/directadmin/data/admin/ip.list
+{ip}
+EOF
+
+cp /usr/local/directadmin/data/users/admin/ip.list /usr/local/directadmin/data/users/admin/ip.list_1
+truncate -s 0 /usr/local/directadmin/data/users/admin/ip.list
+cat <<EOF > /usr/local/directadmin/data/users/admin/ip.list
+{ip}
+EOF
+
+cp /usr/local/directadmin/data/users/admin/user_ip.list /usr/local/directadmin/data/users/admin/user_ip.list_1
+truncate -s 0 /usr/local/directadmin/data/users/admin/user_ip.list
+cat <<EOF > /usr/local/directadmin/data/users/admin/user_ip.list
+{ip}
+EOF
+
+cp /usr/local/directadmin/data/users/admin/user.conf /usr/local/directadmin/data/users/admin/user.conf_1
+truncate -s 0 /usr/local/directadmin/data/users/admin/user.conf 
+cat <<EOF > /usr/local/directadmin/data/users/admin/user.conf 
+account=ON
+additional_bandwidth=0
+aftp=ON
+api_with_password=yes
+bandwidth=unlimited
+catchall=ON
+cgi=ON
+creator=root
+cron=ON
+date_created=Sat Sep  4 13:45:58 2021
+dnscontrol=ON
+docsroot=./data/skins/evolution
+domainptr=unlimited
+email=admin@{redirect_host}
+ftp=unlimited
+inode=unlimited
+ip=51.79.255.86
+language=en
+login_keys=ON
+mysql=unlimited
+name=admin
+nemailf=unlimited
+nemailml=unlimited
+nemailr=unlimited
+nemails=unlimited
+notify_on_all_question_failures=yes
+notify_on_all_twostep_auth_failures=yes
+ns1=ns1.{redirect_host}
+ns2=ns2.{redirect_host}
+nsubdomains=unlimited
+package=admin
+php=ON
+quota=unlimited
+security_questions=no
+sentnotice_quota_full=no
+sentwarning_bandwidth=no
+sentwarning_inode=no
+sentwarning_quota=no
+skin=evolution
+spam=ON
+ssh=ON
+ssl=ON
+suspend_at_limit=OFF
+suspended=no
+sysinfo=ON
+twostep_auth=no
+user_widgets=WGT_USER_STATS:WGT_ADDITIONAL_DOMAINS
+username=admin
+usertype=admin
+vdomains=unlimited
+zoom=100
+EOF
+
+cp /usr/local/directadmin/data/users/admin/reseller.conf /usr/local/directadmin/data/users/admin/reseller.conf_1
+truncate -s 0 /usr/local/directadmin/data/users/admin/reseller.conf
+cat <<EOF > /usr/local/directadmin/data/users/admin/reseller.conf
+additional_bandwidth=0
+aftp=ON
+bandwidth=unlimited
+catchall=ON
+cgi=ON
+clamav=ON
+cron=ON
+dns=OFF
+dnscontrol=ON
+domainptr=unlimited
+ftp=unlimited
+git=ON
+inode=unlimited
+ip=shared
+ips=0
+login_keys=ON
+mysql=unlimited
+nemailf=unlimited
+nemailml=unlimited
+nemailr=unlimited
+nemails=unlimited
+ns1=ns1.{redirect_host}
+ns2=ns2.{redirect_host}
+nsubdomains=unlimited
+nusers=unlimited
+oversell=OFF
+package=admin
+php=ON
+quota=unlimited
+redis=ON
+reseller_widgets=WGT_RESELLER_STATS:WGT_LIST_USERS:WGT_SKINS
+serverip=ON
+spam=ON
+ssh=ON
+ssl=ON
+subject=Your account for |domain| is now ready for use.
+sysinfo=ON
+userssh=ON
+vdomains=unlimited
+wordpress=ON
+EOF
+
+cp /usr/local/directadmin/data/users/admin/php/php-fpm72.conf /usr/local/directadmin/data/users/admin/php/php-fpm72.conf_1
+truncate -s 0 /usr/local/directadmin/data/users/admin/php/php-fpm72.conf
+cat <<EOF > /usr/local/directadmin/data/users/admin/php/php-fpm72.conf
+[admin]
+user = $pool
+group = $pool
+listen = /usr/local/php72/sockets/$pool.sock
+listen.owner = $pool
+listen.group = apache
+listen.mode = 660
+pm = ondemand
+pm.max_children = 10
+pm.process_idle_timeout = 20
+pm.max_requests = 500
+php_admin_value[sendmail_path] = /usr/sbin/sendmail -t -i -f admin@{redirect_host}
+php_admin_value[session.save_path] = /home/admin/tmp
+php_admin_value[open_basedir] = /home/admin/:/tmp/:/var/tmp/:/opt/alt/php72/usr/share/pear/:/dev/urandom:/usr/local/php72/lib/:/usr/local/php72/lib/:/usr/local/php80/lib/:/usr/local/php81/lib/:/usr/local/php82/lib/:/usr/local/lib/php/
+php_admin_value[mail.log] = /home/admin/.php/php-mail.log
+security.limit_extensions = .php .php52 .php53 .php54 .php55 .php56 .php60 .php70 .php71 .phtml .inc .php72
+EOF
+
 mv /usr/local/directadmin/data/admin/show_all_users.cache /usr/local/directadmin/data/admin/show_all_users.cache_1
-cp /usr/local/directadmin.bak-old/data/admin/ip.list /usr/local/directadmin/data/admin/ip.list
-cp /usr/local/directadmin.bak-old/data/users/admin/ip.list /usr/local/directadmin/data/users/admin/ip.list
-cp /usr/local/directadmin.bak-old/data/users/admin/reseller.conf /usr/local/directadmin/data/users/admin/reseller.conf
-cp /usr/local/directadmin.bak-old/data/users/admin/user_ip.list /usr/local/directadmin/data/users/admin/user_ip.list
+
+/usr/local/directadmin/scripts/ipswap.sh 103.110.84.30 {ip}
+
+truncate -s 0 /usr/local/directadmin/custombuild/custombuild.log
 `;
 
-const networkConfig=`#Auth: wptop.net
-
-# Remove ifcfg-lo:100 configuration
+const networkConfig=`# Remove ifcfg-lo:100 configuration
 rm -rf /etc/sysconfig/network-scripts/ifcfg-lo:100
 rm -rf /etc/sysconfig/network-scripts/ifcfg-eth0:100
 
